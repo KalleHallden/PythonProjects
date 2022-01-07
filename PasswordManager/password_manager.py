b9c0469f8a26c2201270d79d25f80496aa7d624d
Mahmoud Hawash <73611543+mahmoud-hawash@users.noreply.github.com>
2022-01-07 08:17:53 +0200

    Improving Login mechanism

--- PasswordManager/password_manager.py
+++ PasswordManager/password_manager.py
@@ -1,24 +1,28 @@
-
 import sqlite3
 from hashlib import sha256
+from getpass import getpass
 
+# ADMIN_PASSWORD = th1s1smyp4$$w0rd
+ADMIN_PASSWORD = "14388f0020454aea5d7752a9e0848eb9d64e1e7267c668d9695a2c13dcfd4cc1"
 
-ADMIN_PASSWORD = "123456"
-
-connect = input("What is your password?\n")
+connect = sha256(getpass("What is your password?\n").encode('utf-8')).hexdigest()
 
 while connect != ADMIN_PASSWORD:
-    connect = input("What is your password?\n")
+    connect = sha256(getpass("What is your password?\n").encode('utf-8')).hexdigest()
     if connect == "q":
         break
 
 conn = sqlite3.connect('pass_manager.db')
 
+
 def create_password(pass_key, service, admin_pass):
-    return sha256(admin_pass.encode('utf-8') + service.lower().encode('utf-8') + pass_key.encode('utf-8')).hexdigest()[:15]
+    return sha256(admin_pass.encode('utf-8') + service.lower().encode('utf-8') + pass_key.encode('utf-8')).hexdigest()[
+           :15]
+
 
 def get_hex_key(admin_pass, service):
     return sha256(admin_pass.encode('utf-8') + service.lower().encode('utf-8')).hexdigest()
+
 
 def get_password(admin_pass, service):
     secret_key = get_hex_key(admin_pass, service)
@@ -29,13 +33,18 @@ def get_password(admin_pass, service):
         file_string = row[0]
     return create_password(file_string, service, admin_pass)
 
+
 def add_password(service, admin_pass):
     secret_key = get_hex_key(admin_pass, service)
+    command = 'INSERT INTO KEYS (PASS_KEY) VALUES (%s);' % ('"' + secret_key + '"')
+    try:
+        conn.execute(command)
+    except:
+        raise ValueError(f'{service} password is already saved.')
 
-    command = 'INSERT INTO KEYS (PASS_KEY) VALUES (%s);' %('"' + secret_key +'"')        
-    conn.execute(command)
     conn.commit()
     return create_password(secret_key, service, admin_pass)
+
 
 if connect == ADMIN_PASSWORD:
     try:
@@ -44,15 +53,14 @@ if connect == ADMIN_PASSWORD:
         print("Your safe has been created!\nWhat would you like to store in it today?")
     except:
         print("You have a safe, what would you like to do today?")
-    
-    
+
     while True:
-        print("\n"+ "*"*15)
+        print("\n" + "*" * 15)
         print("Commands:")
         print("q = quit program")
         print("gp = get password")
         print("sp = store password")
-        print("*"*15)
+        print("*" * 15)
         input_ = input(":")
 
         if input_ == "q":
@@ -62,7 +70,7 @@ if connect == ADMIN_PASSWORD:
             print("\n" + service.capitalize() + " password created:\n" + add_password(service, ADMIN_PASSWORD))
         if input_ == "gp":
             service = input("What is the name of the service?\n")
-            print("\n" + service.capitalize() + " password:\n"+get_password(ADMIN_PASSWORD, service))
+            print("\n" + service.capitalize() + " password:\n" + get_password(ADMIN_PASSWORD, service))
 
 
 
